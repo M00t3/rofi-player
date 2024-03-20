bfeda9560423785727505d26186d5af427b5ebd5
Mostafa Tehrani <mostafa.tehrani.dev@gmail.com>
2024-03-20 17:50:09 +0200

    clean the code

--- player.sh
+++ player.sh
@@ -6,9 +6,17 @@ cd "$SCRIPT_DIR" || exit 2
 # load config file
 source ./config.conf
 
-selected_playlist=$(sed '/#.*/d;/^$/d' ./playlists_and_musics.csv | awk -F',' '{print $1}' | rofi -dmenu -i -lines 10 -width 30 -p "Select Music or Playlist: ")
-link=$(sed '/#.*/d;/^$/d' ./playlists_and_musics.csv | grep "^$selected_playlist," | awk -F',' '{print $2}')
-is_url_link=$(sed '/#.*/d;/^$/d' ./playlists_and_musics.csv | grep "$selected_playlist" | awk -F',' '{print $3}')
+selected_playlist=$(sed '/#.*/d;/^$/d' ./playlists_and_musics.csv |
+	awk -F',' '{print $1}' |
+	rofi -dmenu -i -lines 10 -width 30 -p "Select Music or Playlist: ")
+
+link=$(sed '/#.*/d;/^$/d' ./playlists_and_musics.csv |
+	grep "^$selected_playlist," |
+	awk -F',' '{print $2}')
+
+is_url_link=$(sed '/#.*/d;/^$/d' ./playlists_and_musics.csv |
+	grep "$selected_playlist" |
+	awk -F',' '{print $3}')
 
 # Isn't enter in rofi
 if [[ ! "$selected_playlist" ]]; then
